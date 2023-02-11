a58c6403edde7fda3a3811d9d10463158e2f8816
Nick Tallant <ndtallant@gmail.com>
2023-02-11 14:53:02 -0600

    Ternary was a good suggestion

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,19 +2,12 @@ import React from "react";
 import { range } from "../../utils";
 
 function Guess({ value }) {
-  if (!value) {
-    return (
-      <p className="guess">{
-        range(5).map(idx => (
-          <span key={idx} className="cell"></span>
-        ))}
-      </p>
-    );
-  }
   return (
       <p className="guess">{
-        value.split('').map((char, idx) => (
-          <span key={idx} className="cell">{char}</span>
+        range(5).map(idx => (
+          <span key={idx} className="cell">{
+            value ? value[idx]: undefined
+          }</span>
         ))}
       </p>
   );
