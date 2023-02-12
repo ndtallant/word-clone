24bd263f842fd861d2b922e1b288a3b5bbcf385c
Nick Tallant <ndtallant@gmail.com>
2023-02-11 18:58:51 -0600

    All done

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import GuessForm  from '../GuessForm/';
 import GuessResults  from '../GuessResults/';
+import WinBanner from '../WinBanner/';
+import LoseBanner from '../LoseBanner/';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,13 +14,25 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-
-
+  // 'playing', 'won', or 'lost'
+  const [gameState, setGameState] = React.useState('playing');
   const [results, setResults] = React.useState([]);
+
+  function submitGuess(currentResult) {
+    const nextResults = [...results, currentResult];
+    setResults(nextResults);
+    if (currentResult.toUpperCase() === answer) {
+      setGameState('won');
+    } else if (nextResults.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameState('lost');
+    }
+  }
   return (
       <>
       <GuessResults results={results} answer={answer}/>
-      <GuessForm results={results} setResults={setResults}/>
+      <GuessForm gameState={gameState} results={results} submitGuess={submitGuess}/>
+      {gameState === 'won' && <WinBanner numGuesses={results.length}/>}
+      {gameState === 'lost' && <LoseBanner answer={answer}/>}
       </>
   );
 }
